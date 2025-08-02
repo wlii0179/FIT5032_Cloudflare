1badf1fc87fc29a2381180859d87e243134306eb
wlii0179 <wlii0179@student.monash.edu>
2025-08-02 13:29:31 +0800

    \

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FIT5032_Cloudflare/'
-    : '/'
-} 
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/<FIT5032_Cloudflare>/'
+      : '/'
+  }
