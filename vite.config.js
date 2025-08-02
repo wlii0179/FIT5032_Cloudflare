86fe46d9e9db0c85de4851ac90a16dea55f8f0ca
wlii0179 <wlii0179@student.monash.edu>
2025-08-02 13:11:33 +0800

    修改vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/<FIT5032_Cloudflare>/'
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_Cloudflare/'
     : '/'
-}
+})
