60fdb8c7a3955f6e239644ab5bba816f8e81374f
wlii0179 <wlii0179@student.monash.edu>
2025-08-02 13:24:41 +0800

    vue.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: process.env.NODE_ENV === 'production'
-    ? '/FIT5032_Cloudflare/'
-    : '/'
+  }
 }) 
